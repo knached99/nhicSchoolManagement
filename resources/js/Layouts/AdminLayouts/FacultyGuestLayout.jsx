39570c501d57333c9e4d0b411a6705fd017204eb
Khaled Nached <khalednached@gmail.com>
2024-02-26 17:05:07 -0500

    Changed background color on guest page

--- resources/js/Layouts/AdminLayouts/FacultyGuestLayout.jsx
+++ resources/js/Layouts/AdminLayouts/FacultyGuestLayout.jsx
@@ -6,7 +6,7 @@ export default function FacultyGuestLayout({ children }) {
       <>
       <HomeNav/>
       <div className="bg-gradient-to-tr from-purple-600 to-sky-500 h-screen w-full flex justify-center items-center">
-   <div className="dark:bg-slate-800 bg-indigo-100 w-full sm:w-1/2 md:w-9/12 lg:w-1/2 shadow-md flex flex-col md:flex-row items-center mx-5 sm:m-0 rounded">
+   <div className="dark:bg-slate-800 bg-white w-full sm:w-1/2 md:w-9/12 lg:w-1/2 shadow-md flex flex-col md:flex-row items-center mx-5 sm:m-0 rounded">
       <div className="w-full md:w-1/2 hidden md:flex flex-col justify-center items-center text-black dark:text-white">
         <h1 className="text-3xl">NHIC</h1>
         <p className="text-5xl font-extrabold">Faculty Portal</p>
